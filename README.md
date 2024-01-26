348f322e2c2b8d9e35bd1a581ca2748182ac39fc
zeni18 <hi.zero.im@gmail.com>
2024-01-27 07:21:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Flutter woo sdk 3.3.2 版本
+
+## 环境
+
+```
+Flutter 3.3.2 • channel stable • https://github.com/flutter/flutter.git
+Framework • revision e3c29ec00c (7 天前) • 2022-09-14 08:46:55 -0500
+Engine • revision a4ff2c53d8
+Tools • Dart 2.18.1 • DevTools 2.15.0
+```
---
+++ lib/pages/my/order_details/controller.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_woo_commerce_getx_learn/common/index.dart';
+import 'package:get/get.dart';
+
+class OrderDetailsController extends GetxController {
+  OrderDetailsController();
+
+  // 订单详情
+  final OrderModel order = Get.arguments as OrderModel;
+
+  _initData() {
+    update(["order_details"]);
+  }
+
+  void onTap() {}
+
+  // @override
+  // void onInit() {
+  //   super.onInit();
+  // }
+
+  @override
+  void onReady() {
+    super.onReady();
+    _initData();
+  }
+
+  // @override
+  // void onClose() {
+  //   super.onClose();
+  // }
+
+  // @override
+  // void dispose() {
+  //   super.dispose();
+  // }
+}
